9ad978c8012ced163ccd91728d6eb0e7b6655063
Abinash-8892 <87180988+Abinash-8892@users.noreply.github.com>
2022-07-21 12:49:03 +0530

    Add files via upload

---
+++ vscodedotnet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace vscodedotnet 
+{
+    class Program
+    {
+        static void Main(string[] args)
+             {
+                 Console.WriteLine("Hello World");
+             }
+
+    }
+    
+}
